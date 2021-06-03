e16f280dc9fb84ac71ae4d94324d5285265db5d4
George Saker <georgesaker147@gmail.com>
2021-06-03 18:41:38 +0100

    added dotfiles

---
+++ config/polybar/scripts/launcher.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rofi -opacity "50" -no-config -no-lazy-grab -show drun -modi drun -theme "$HOME/.config/polybar/menu"/launcher.rasi
+
---
+++ misc/.profile
@@ -0,0 +1,7 @@
+export PATH="$HOME/scripts:$PATH"
+
+alias ll="ls -l"
+alias la="ls -al"
+alias c="clear"
+alias pac="sudo pacman"
+alias aur="pikaur"
