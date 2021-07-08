8a3e83a0d7f86fb7c1d9047a303f51577b020725
George Saker <georgesaker147@gmail.com>
2021-07-08 19:33:45 +0100

    Updated config files

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,5 +8,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -q main -c ~/.config/polybar/config.ini &
+  done
+else
+  polybar -q main -c ~/.config/polybar/config.ini &
+fi
 # Launch the bar
-polybar -q main -c ~/.config/polybar/config.ini &
+
